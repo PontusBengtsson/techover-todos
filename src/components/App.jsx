328af87701dbea8c2ee021c37227a2f89ddab688
PontusBengtsson <pontus.bengtsson@live.com>
2024-03-27 21:58:56 +0100

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+import Input from './Input';
+import List from './List';
+
+
+function App() {
+	const [todo, setTodo] = useState('');
+	const [todos, setTodos] = useState([]);
+
+	const addTodo  = () => {
+		if(todo !== '') {
+			setTodos ( [...todos,todo ]);
+			setTodo('')
+		}
+		
+	}
+
+	const complete = (text) => {
+		const uncompletedTodos = todos.filter((todo) => todo !== text);
+		setTodos(uncompletedTodos);
+	}
+	 
+	return (
+		<div className="App">
+			<img className="logo" src="/logo.png" alt="techover" />
+			<Input setTodo={setTodo} todo={todo} addTodo={addTodo} />
+			<List todos = {todos} complete ={complete} />
+		</div>
+	);
+}
+
+export default App;
